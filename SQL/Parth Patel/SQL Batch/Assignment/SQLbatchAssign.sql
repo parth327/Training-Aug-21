2c25538e474a9d9b754d3a27a7026c9239a047b3
Parth <pp744336@gmail.com>
2021-09-02 11:05:09 +0530

    Modified

--- SQL/Parth Patel/SQL Batch/Assignment/SQLbatchAssign.sql
+++
@@ -1,7 +0,0 @@
---SQL Batch Assignment
-
-
---Create a batch Select Banking as ‘Bank Dept’, Insurance as ‘Insurance Dept’ and Services as ‘Services Dept’ from employee table
-
- SELECT case DepartmentName when 'IT' then 'Information technology' when 'Banking' then 'Bank Dept' when 'Insurance' then 'Insurance Dept' when
-'Services' then 'Services Dept' END as Department FROM Departments
