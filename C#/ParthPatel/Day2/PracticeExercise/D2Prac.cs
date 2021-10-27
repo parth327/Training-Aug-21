d72c41b86d1a1c6e2ce0353c95e051009be70ea7
Parth <pp744336@gmail.com>
2021-10-27 11:55:58 +0530

    REFS : #1797 and fixes #1797

---
+++ C#/ParthPatel/Day2/PracticeExercise/D2Prac.cs
@@ -0,0 +1,89 @@
+﻿//Day 2 Practice
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp2
+{
+   
+
+    //Class
+    //class Car
+    //{
+    //    string color = "red";
+
+    //    static void Main(string[] args)
+    //    {
+    //        Car myObj = new Car();
+    //        Console.WriteLine(myObj.color);
+    //        Console.ReadLine();
+    //    }
+    //}
+    //Multiple Class
+    //class Program
+    //{
+    //    static void Main(string[] args)
+    //    {
+    //        Car myObj = new Car();
+    //        Console.WriteLine(myObj.color);
+    //        Console.ReadLine();
+    //    }
+    //}
+    //class Car
+    //{
+    //    public string color = "blue";
+    //}
+    //Properties
+    //class Program
+    //{
+    //    static void Main(string[] args)
+    //    {
+    //        Person myObj = new Person();
+    //        myObj.Name = "Parth";
+    //        Console.WriteLine(myObj.Name);
+    //        Console.ReadLine();
+    //    }
+    //}
+    //class Person
+    //{
+    //    public string Name
+    //    { get; set; }
+    //}
+    //Constructor
+    //class Car
+    //{
+    //    public string model;
+
+    //    // Create a class constructor with a parameter
+    //    public Car(string modelName)
+    //    {
+    //        model = modelName;
+    //    }
+
+    //    static void Main(string[] args)
+    //    {
+    //        Car Ford = new Car("Mustang");
+    //        Console.WriteLine(Ford.model);
+    //        Console.ReadLine();
+
+    //    }
+    //}
+    //Static
+    //class Test
+    //{
+    //    static int x = y;
+    //    static int y = 5;
+
+    //    static void Main()
+    //    {
+    //        Console.WriteLine(Test.x);
+    //        Console.WriteLine(Test.y);
+
+    //        Test.x = 99;
+    //        Console.WriteLine(Test.x);
+    //        Console.ReadKey();
+    //    }
+    //}
+}
