5aeaf6eff2911c14ca3c239d98bdd09cf1549428
Parth <pp744336@gmail.com>
2021-10-27 12:00:02 +0530

    REFS : #1837 and fixes #1837

---
+++ C#/ParthPatel/Day3/Assigment/d3Assign.cs
@@ -0,0 +1,135 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace d3Assign
+{
+    //Implement all the oops concept for Employee payroll system. Create above class hierarchy, implement inheritance and polymorphism.
+    interface IEmp
+    {
+        void CreateEmployee();
+        void Salary();
+        void Display();
+    }
+
+    abstract class Employee : IEmp
+    {
+        static int EmployeeID;
+        string Name, Address, PanNum;
+
+        public virtual void CreateEmployee()
+        {
+            EmployeeID++;
+            Console.Write("Enter Name: ");
+            Name = Console.ReadLine();
+            Console.Write("Enter Address: ");
+            Address = Console.ReadLine();
+            Console.Write("Enter pan Number: ");
+            PanNum = Console.ReadLine();
+        }
+
+        public virtual void Display()
+        {
+            Console.WriteLine($"EmployeeId  {EmployeeID} Name is {Name} Address is {Address} PanNum is {PanNum} ");
+        }
+
+
+        public abstract void Salary();
+
+    }
+
+    class PartTime : Employee
+    {
+        int NoOfHours, TotalSales;
+        float SalesPerHour;
+
+        public override void CreateEmployee()
+        {
+            base.CreateEmployee();
+            Console.Write("Enter No. Of Hours: ");
+            NoOfHours = Convert.ToInt32(Console.ReadLine());
+            Console.Write("Enter TotalSales: ");
+            TotalSales = Convert.ToInt32(Console.ReadLine());
+            SalesPerHour = (float)TotalSales / NoOfHours;
+        }
+
+        public override void Salary()
+        {
+            Console.WriteLine($"The Salary of Employee is Rs{NoOfHours * 40 + SalesPerHour * 500}");
+        }
+
+
+    }
+
+    class FullTime : Employee
+    {
+        decimal Basic, HRA, TA, DA;
+
+        public override void CreateEmployee()
+        {
+            base.CreateEmployee();
+            Console.Write("Enter base pay: ");
+            Basic = Convert.ToDecimal(Console.ReadLine());
+            Console.Write("Enter Rent if Employee living in rental accomodation? Or enter 0.");
+            HRA = Convert.ToDecimal(Console.ReadLine());
+            Console.Write("Enter TA and Da: ");
+            TA = Convert.ToDecimal(Console.ReadLine());
+            DA = Convert.ToDecimal(Console.ReadLine());
+
+
+        }
+
+        public override void Salary()
+        {
+            Console.WriteLine($"total salary of the employee is {Basic + HRA + TA + DA}");
+        }
+
+    }
+
+
+    class Program
+    {
+        public void employeeIntialize(IEmp emp)
+        {
+            if (emp.GetType().Name == "PartTime")
+            {
+                PartTime partTime = (PartTime)emp;
+                partTime.CreateEmployee();
+                emp.Salary();
+
+            }
+            else if (emp.GetType().Name == "FullTime")
+            {
+                FullTime fullTime = (FullTime)emp;
+                fullTime.CreateEmployee();
+                emp.Salary();
+            }
+        }
+        static void Main(string[] args)
+        {
+            IEmp employee = null;
+            Console.WriteLine("Enter 1 for part time and 2 full time employee");
+            int choice = Convert.ToInt32(Console.ReadLine());
+            switch (choice)
+            {
+                case 1:
+                    employee = new PartTime();
+                    break;
+                case 2:
+                    employee = new FullTime();
+                    break;
+                default:
+                    Console.WriteLine("Invalid choice");
+                    break;
+            }
+
+            var program = new Program();
+            program.employeeIntialize(employee);
+            Console.ReadLine();
+
+
+        }
+    }
+}
