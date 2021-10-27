9ba599bdf461c849722def68ee5c99bfc1b9ca5f
Parth <pp744336@gmail.com>
2021-10-27 11:51:27 +0530

    REFS : #1774 and fixes #1774

---
+++ C#/ParthPatel/Day1/Practice Exercise/d1Practice.cs
@@ -0,0 +1,82 @@
+﻿//Day 1 Practice
+
+using System;
+
+namespace D1assign
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+            //int a = 42;
+            //int b = 119;
+            //int c = a + b;
+            //Console.WriteLine(c);
+            //Console.ReadKey();
+            // Declare variables and then initialize to zero.
+            //int num1 = 0; int num2 = 0;
+
+            // Display title as the C# console calculator app.
+            //Console.WriteLine("Console Calculator in C#\r");
+            //Console.WriteLine("------------------------\n");
+
+            // Ask the user to type the first number.
+            //Console.WriteLine("Type a number, and then press Enter");
+            //num1 = Convert.ToInt32(Console.ReadLine());
+
+            // Ask the user to type the second number.
+            //Console.WriteLine("Type another number, and then press Enter");
+            //num2 = Convert.ToInt32(Console.ReadLine());
+
+            // Ask the user to choose an option.
+            //Console.WriteLine("Choose an option from the following list:");
+            //Console.WriteLine("\ta - Add");
+            //Console.WriteLine("\ts - Subtract");
+            //Console.WriteLine("\tm - Multiply");
+            //Console.WriteLine("\td - Divide");
+            //Console.Write("Your option? ");
+
+            // Use a switch statement to do the math.
+            //switch (Console.ReadLine())
+            //{
+            //    case "a":
+            //        Console.WriteLine($"Your result: {num1} + {num2} = " + (num1 + num2));
+            //        break;
+            //    case "s":
+            //        Console.WriteLine($"Your result: {num1} - {num2} = " + (num1 - num2));
+            //        break;
+            //    case "m":
+            //        Console.WriteLine($"Your result: {num1} * {num2} = " + (num1 * num2));
+            //        break;
+            //    case "d":
+            //        while (num2 == 0)
+            //        {
+            //            Console.WriteLine("Enter a non-zero divisor: ");
+            //            num2 = Convert.ToInt32(Console.ReadLine());
+            //        }
+            //        Console.WriteLine($"Your result: {num1} / {num2} = " + (num1 / num2));
+            //        break;
+            // Ask the user to enter a non-zero divisor until they do so.
+
+
+
+            // Wait for the user to respond before closing.
+            //Console.Write("Press any key to close the Calculator console app...");
+            //Console.ReadKey();
+
+         //Operators and Expressions
+            //var r = 2.3;
+            //var message = $"The area of a circle with radius {r} is {Math.PI * r * r:F3}.";
+            //Console.WriteLine(message);
+        //Operator Precedence   
+            //var a = 2 + 2 * 2;
+            //Console.WriteLine(a);
+        //Operator associativity
+            //int a = 13 / 5 / 2;
+            //int b = 13 / (5 / 2);
+            //Console.WriteLine($"a = {a}, b = {b}");  // output: a = 1, b = 6
+            
+        }
+    }
+}
