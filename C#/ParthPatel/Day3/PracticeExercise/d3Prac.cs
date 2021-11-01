1a85b00c9fa46df8195774954581be8f05f99242
Parth <pp744336@gmail.com>
2021-11-01 09:08:17 +0530

    REFS : #1935 and fixes #1935

---
+++ C#/ParthPatel/Day3/PracticeExercise/d3Prac.cs
@@ -0,0 +1,110 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace d3Prac
+{
+    //Inheritance
+    //class Program
+    //{
+    //    static void Main(string[] args)
+    //    {
+    //        Car myCar = new Car();
+    //        myCar.honk();
+    //        Console.WriteLine(myCar.brand + " " + myCar.modelName);
+    //    }
+    //}
+    //class Car : Vehicle 
+    //{
+    //    public string modelName = "Mustang";  
+    //}
+    //class Vehicle 
+    //{
+    //    public string brand = "Ford";  
+    //    public void honk()             
+    //    {
+    //        Console.WriteLine("Tuut, tuut!");
+    //        Console.ReadLine();
+    //    }
+    //}
+    //Polymorphism
+    //class Program
+    //{
+    //    static void Main(string[] args)
+    //    {
+    //        Animal myAnimal = new Animal();
+    //        Animal myPig = new Pig();  
+    //        Animal myDog = new Dog();  
+    //        myAnimal.animalSound();
+    //        myPig.animalSound();
+    //        myDog.animalSound();
+    //    }
+    //}
+    //class Animal   
+    //{
+    //    public void animalSound()
+    //    {
+    //        Console.WriteLine("The animal makes a sound");
+    //    }
+    //}
+
+    //class Pig : Animal 
+    //{
+    //    public void animalSound()
+    //    {
+    //        Console.WriteLine("The pig says: wee wee");
+    //    }
+    //}
+
+    //class Dog : Animal 
+    //{
+    //    public void animalSound()
+    //    {
+    //        Console.WriteLine("The dog says: bow wow");
+    //    }
+    //}
+    //Encapsulation
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            User u = new User();
+            u.Name = "Parth Patel";
+            u.Location = "Ahmedabad";
+            Console.WriteLine("Name: " + u.Name);
+            Console.WriteLine("Location: " + u.Location);
+            Console.WriteLine("\nPress Enter Key to Exit..");
+            Console.ReadLine();
+        }
+    }
+    class User
+    {
+        private string location;
+        private string name;
+        public string Location
+        {
+            get
+            {
+                return location;
+            }
+            set
+            {
+                location = value;
+            }
+        }
+        public string Name
+        {
+            get
+            {
+                return name;
+            }
+            set
+            {
+                name = value;
+            }
+        }
+    }
+
+}
---
+++ C#/ParthPatel/Day6/Assignment/d6Assign.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+//public delegate int AreaDelegate(int l, int b);
+namespace d6Assign
+{
+    //class Program
+    //{
+    //    static void Main(string[] Args)
+    //    {
+    //        //Assignment
+    //        //1.Compute area of rectangle using func delegate
+    //        AreaDelegate x = new AreaDelegate(AreaRec);
+    //        var result = x.Invoke(10, 20);
+    //        Console.WriteLine("Area Of Rectangle :" + result);
+    //        Console.ReadLine();
+    //        Delegatedemo demo = new Delegatedemo();
+    //        AreaDelegate y = new AreaDelegate(demo.MulNum);
+    //        var resultdelegatedemo = y.Invoke(30, 20);
+    //        Console.WriteLine("Area of Rectangle :" + resultdelegatedemo);
+    //        Console.ReadLine();
+    //    }
+    //    public static int AreaRec(int l, int b) {
+    //        return l * b;
+    //    }
+    //}
+    //class Delegatedemo
+    //{
+    //    public int MulNum(int l, int b)
+    //    {
+    //        return l * b;
+    //    }
+    //}
+    //2. Compute add of two number using lambda expression
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Func<double, double, double> area = (double height, double widtht) => (height * widtht);
+            Console.WriteLine(area(6.3, 5.2));
+            Func<int, int, int> sum = (int num1, int num2) => (num1 + num2);
+            Console.WriteLine(sum(2, 3));
+            Console.ReadKey();
+        }
+    }
+}
