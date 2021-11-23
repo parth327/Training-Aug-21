f1e3ef689875c3aa0362f53e54984d6a2bafebc8
Parth <pp744336@gmail.com>
2021-11-23 13:39:08 +0530

    REFS : #2185 and fixes #2185

---
+++ TypeScript/ParthPatel/Day1/Assignment/d1.ts
@@ -0,0 +1,63 @@
+// Store 5 employees’ data in one array (ID,FirstName,LastName,Address,Salary). 
+// Do the operation searching by indexnumber, EmployeeID, Insert the employee, 
+// delete the employee from the Array. Create one more array emp and join the value 
+// with above array. When display list combine firstname and lastname as fullname,
+//  From the address field flatnumber,city,state and should be splited.
+// PF should be computed and total salary should be display.
+ 
+type employee = {Id:Number,FirstName:String,LastName:String,Address:String,Salary:Number}
+var arr:employee[] = [];
+
+// push operation
+arr.push({Id:1,FirstName:"Parth",LastName:"Patel",Address:"naroda,Ahmedabad,Gujarat",Salary:25000})
+arr.push({Id:2,FirstName:"Rohit",LastName:"Sharma",Address:"Sola,Ahmedabad,Gujarat",Salary:20000})
+arr.push({Id:3,FirstName:"Abhay",LastName:"Vanzara",Address:"Jaipur,Udaipur,Rajasthan",Salary:30000})
+arr.push({Id:4,FirstName:"Mohan",LastName:"Vaghela",Address:"Tekra,Panchmahal,gujarat",Salary:25000})
+arr.push({Id:5,FirstName:"Saurin",LastName:"Patel",Address:"vastrapur,Ahmedabad,Gujarat",Salary:30000})
+
+//searching operation
+// search by index
+// function searchByIndex (array,index) : employee {
+//     if(index >= array.length){
+//         return null;
+//     }
+//     return arr[index];
+// }
+// var _4thEmployee: employee = searchByIndex(arr,3);
+// console.log(_4thEmployee);
+
+//search by Id
+// function searchByEmployeeId(array,key) : employee {
+//     return array.filter(emp => emp.Id === key)[0];
+// }
+// var employeeWithId2 = searchByEmployeeId(arr,2);
+// console.log(employeeWithId2);
+
+//insert employee
+// function InsertEmployee(employee) :void {
+//     arr.push(employee);
+// }
+// InsertEmployee({Id:6,FirstName:"Purohit",LastName:"Khanna",Address:"Dang",Salary:15000});
+// console.log(arr);
+
+//delete employee
+// function DeleteEmployeebyId(Id:number) : void {
+//     arr = arr.filter(employee => employee.Id !== Id);
+// }
+// DeleteEmployeebyId(6);
+// console.log(arr);
+
+// Create one more array emp and join the value with above array
+var arr2:employee[] = [];
+arr2.push({Id:7,FirstName:"Naman",LastName:"Mathur",Address:"New Delhi,Delhi-NCR,Delhi",Salary:25000})
+arr2.push({Id:8,FirstName:"Rakesh",LastName:"Patel",Address:"Kathvada,Naroda,Gujarat",Salary:30000})
+var arr3 = arr.concat(arr2);
+
+//When display list combine firstname and lastname as fullname, From the address field flatnumber,city,state and should be splited.PF should be computed and total salary should be display
+for(var emp of arr3){
+    console.log(`FullName: ${emp.FirstName} ${emp.LastName} `);
+    var address = emp.Address.split(",");
+    console.log(`City: ${address[0]} Dist: ${address[1]} State : ${address[2]}`);
+    var pf = emp.Salary.valueOf() * 0.12;
+    console.log(`PF : ${pf} TotaSalary: ${emp.Salary.valueOf() - pf}`);
+}
---
+++ TypeScript/ParthPatel/Day1/PracticeExercise/app.ts
@@ -0,0 +1,55 @@
+// Starting
+// console.log("Hello");
+//Variables
+// function Scope()
+// {
+//     if(true)
+//     {
+//         var foo = "use";
+//     }
+// console.log(foo);sc 
+// }
+// Adding Type Annotations
+// let myString = 'THis is a string';
+// function  ReturnNumber() : number
+// {
+//     return 42;
+
+// }
+// let anotherString : string = 'This is also a string';
+// function GetAllBooks(){
+//    let books = [
+//         {title:'Ulysses',author:'James'},
+//     {title:'A',author:'B'}
+// ];
+// return books;
+// };
+
+// function LogFirst(books){
+//     let no = books.length;
+//     for(let currentBook of books)
+//     {
+//         var firstAvailable='';
+//         if(currentBook.available){
+//             firstAvailable = currentBook.title;
+//             break;
+//         }
+//     }
+//     console.log('Total Books:' + no);
+//     console.log('First Available:' + firstAvailable);
+// }
+// const allBooks = GetAllBooks();
+// LogFirst(allBooks);
+// Enum
+// enum Category{Biography,Poetry,Fiction};
+// let favCategory : Category = Category.Biography;
+// console.log(favCategory);
+// let catogoryString = Category[favCategory];
+// Array
+// let strArray1:string[]=['here','are','strings'];
+// let strArray2 : Array<string>=['more','strings','here'];
+// let anyArray : any[] = [42,true,'banana'];
+// //Tuple
+// let myTuple :[number,string]=[25,'truck'];
+// let first = myTuple[0];
+// let second = myTuple[1];
