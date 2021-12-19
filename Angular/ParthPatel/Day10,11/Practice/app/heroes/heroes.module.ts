1913e0876d449be22177246079decde657a5e5b9
parth327 <82134704+parth327@users.noreply.github.com>
2021-12-19 16:46:34 +0530

    REFS : #2567 and fixes #2567

---
+++ Angular/ParthPatel/Day10,11/Practice/app/heroes/heroes.module.ts
@@ -0,0 +1,23 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { FormsModule } from '@angular/forms';
+import { HeroListComponent } from '../hero-list/hero-list.component';
+// import { HeroDetailComponent } from '../;
+
+import { HeroesRoutingModule } from './heroes-routing.module';
+
+
+@NgModule({
+  declarations: [
+    HeroListComponent,
+    // HeroDetailComponent
+
+  ],
+  imports: [
+    CommonModule,
+    FormsModule,
+   
+    HeroesRoutingModule
+  ]
+})
+export class HeroesModule { }
